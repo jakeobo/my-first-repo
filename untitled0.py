9291ae995232400071bb789a00d98c0061f4ef09
jakeobo <56733461+jakeobo@users.noreply.github.com>
2020-01-28 15:04:57 +0100

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -6,8 +6,6 @@ Created on Thu Jan 23 14:39:19 2020
 """
 
 def hello(a, b, c):
-<<<<<<< HEAD
+    print("HELLoooOo")
     return (a-b)**c
-=======
-    return (a**b)**c
->>>>>>> dd1f472f1b465329c0ba4f1bbc08948e399c7214
+
